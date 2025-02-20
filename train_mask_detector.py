c02ca3396ad974a30190e19cdd390dc28c268035
Rohitha Yennapu <105338153+Rohitha-Y@users.noreply.github.com>
2025-02-20 13:20:50 +0530

    Update train_mask_detector.py

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -1,3 +1,5 @@
+# Face Mask Detection - Updated by Rohitha Yennapu
+
 # import the necessary packages
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import MobileNetV2
